9f0e4e2b521fb110e814cd5dfe2c0882dcb06bf3
3Mr <3Mr@OMEN>
2026-01-25 16:35:30 +0200

    add product images and header render

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// js/main.js
-import { loadPartial } from "./core/utils";
+// load header and footer partials
+import { loadPartial } from "./core/utils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadPartial("header", "../partials/header.html");
+  loadPartial("header", "../pages/partials/header.html");
   //   loadPartial("footer", "../partials/footer.html");
 });
